ac59203d364750efdf17fdf87790dbdeb85bc8fe
Hkey1 <a.v.belousoff@gmail.com>
2024-10-09 13:17:31 +0200

    add error from hkey-extended-promise

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const tillEvent = require('./tillEvent.js');
-const Emitter   = require('./Emitter.js');
-
-module.exports           = tillEvent;
-module.exports.Emitter   = Emitter;
-module.exports.tillEvent = tillEvent.bind(null);
+const { 
+	TimeoutError, DontFinishError, 
+	PseudoAbortError, isAbortError 
+}                = require('hkey-extended-promise');
+const tillEvent  = require('./tillEvent.js');
+const Emitter    = require('./Emitter.js');
+	
+module.exports                  = tillEvent;
+module.exports.Emitter          = Emitter;
+module.exports.tillEvent        = tillEvent.bind(null);
+module.exports.TimeoutError     = TimeoutError;
+module.exports.DontFinishError  = DontFinishError;
+module.exports.PseudoAbortError = PseudoAbortError;
+module.exports.isAbortError     = isAbortError;
